4c8b70a7db9cb8eb4208865ed0408c14c775a58d
mirazfaruk <mirazfaruk@gmail.com>
2024-05-26 23:22:45 +0600

    Complete

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -5,6 +5,7 @@ import './Countries.css';
 const Countries = () => {
     const [countries, setCountries] = useState([]);
     const [visitedCountries, setVisitedCountries] = useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
@@ -18,7 +19,13 @@ const Countries = () => {
         setVisitedCountries(newVisitedCountries);
     }
 
+    const handleVisitedFlags = (flag) => { 
+        const newVisitedFlags = [...visitedFlags, flag]
+        setVisitedFlags(newVisitedFlags);
+    }
+
     return (
+        // Display Countries and flags
         <div>
             <h3>Countries: {countries.length}</h3>
             <div>
@@ -29,9 +36,15 @@ const Countries = () => {
                     }
                 </ul>
             </div>
+            <div className="flag-container">
+                {
+                    visitedFlags.map((flag, idx) => <img key={idx} src={flag}></img>)
+                }
+            </div>
+
             <div className="country-container">
                 {
-                    countries.map(country => <Country key={country.ccs3} handleVisitedCountry={handleVisitedCountry} country={country}></Country>)
+                    countries.map(country => <Country key={country.ccs3} handleVisitedCountry={handleVisitedCountry} handleVisitedFlags={handleVisitedFlags} country={country}></Country>)
                 }
             </div>
         </div>
